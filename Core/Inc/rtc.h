f95574549d3a9aff087f1f04f74eb09b96462f2d
diligentliu <3391569802@qq.com>
2024-01-24 14:22:56 +0800

    BKP 备份寄存器 & RTC 实时时钟

--- Core/Inc/rtc.h
+++ Core/Inc/rtc.h
@@ -1,30 +1,13 @@
 #ifndef STM32_STD_RTC_H
 #define STM32_STD_RTC_H
 
-#include "stm32f10x.h"
+#include "main.h"
 #include "time.h"
 
-/*
-	OLED_Init();
-	rtc_init();
-	OLED_ShowString(1, 1, "Date: XXXX-XX-XX");
-	OLED_ShowString(2, 1, "Time: XX:XX:XX");
-	OLED_ShowString(3, 1, "CNT : ");
-	while (1) {
-		get_time();
-
-		OLED_ShowNum(1, 7, local_time.tm_year + 1900, 4);
-		OLED_ShowNum(1, 12, local_time.tm_mon + 1, 2);
-		OLED_ShowNum(1, 15, local_time.tm_mday, 2);
-		OLED_ShowNum(2, 7, local_time.tm_hour + 8, 2);
-		OLED_ShowNum(2, 10, local_time.tm_min, 2);
-		OLED_ShowNum(2, 13, local_time.tm_sec, 2);
-		OLED_ShowNum(3, 7, RTC_GetCounter(), 10);
-	}
- */
 extern struct tm local_time;
 void rtc_init();
 void get_time();
 void set_time();
+void rtc_test();
 
 #endif //STM32_STD_RTC_H
--- Core/Src/rtc.c
+++ Core/Src/rtc.c
@@ -46,3 +46,22 @@ void set_time() {
 	RTC_SetCounter(time_cnt);
 	RTC_WaitForLastTask();
 }
+
+void rtc_test() {
+	OLED_Init();
+	rtc_init();
+	OLED_ShowString(1, 1, "Date: XXXX-XX-XX");
+	OLED_ShowString(2, 1, "Time: XX:XX:XX");
+	OLED_ShowString(3, 1, "CNT : ");
+	while (1) {
+		get_time();
+
+		OLED_ShowNum(1, 7, local_time.tm_year + 1900, 4);
+		OLED_ShowNum(1, 12, local_time.tm_mon + 1, 2);
+		OLED_ShowNum(1, 15, local_time.tm_mday, 2);
+		OLED_ShowNum(2, 7, local_time.tm_hour + 8, 2);
+		OLED_ShowNum(2, 10, local_time.tm_min, 2);
+		OLED_ShowNum(2, 13, local_time.tm_sec, 2);
+		OLED_ShowNum(3, 7, RTC_GetCounter(), 10);
+	}
+}
